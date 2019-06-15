bf5686386c943e0a44d74a28b914f4dfb4754da1
Dawid Kaźmierczak <dawid@macbook-pro-dawid.home>
2019-06-15 11:35:46 +0200

    first projects

---
+++ Spring Framework/Dependency/dependency-injection-with-xml/src/main/java/pl/javastart/app/SpringDiApplicationWithXml.java
@@ -0,0 +1,14 @@
+package pl.javastart.app;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.javastart.beans.MessagePrinter;
+
+public class SpringDiApplicationWithXml {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+        MessagePrinter printer = ctx.getBean(MessagePrinter.class);
+        printer.print();
+        ctx.close();
+    }
+}
